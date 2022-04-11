cd59fcf83f58338c76fe60b3fa95a1cb627187a6
Gavrila Cristina Gabriela <cristinagabriela30@yahoo.com>
2022-04-11 13:02:23 +0300

    [Tema6] Configured API Gateway

--- lab-6-api-gateway/service1/src/main/java/com/example/service1/Service1Application.java
+++ lab-6-api-gateway/service1/src/main/java/com/example/service1/Service1Application.java
@@ -2,8 +2,17 @@ package com.example.service1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class Service1Application {
 
 	public static void main(String[] args) {
@@ -16,5 +25,14 @@ public class Service1Application {
 	// 3. print request headers
 	// 4. register the service in eureka
 
+	@ResponseStatus(HttpStatus.OK)
+	@GetMapping("/api/greeting")
+	public String greeting(@RequestHeader HttpHeaders headers, String name){
+		for(String headerKey : headers.keySet()){
+			System.out.println(headerKey + ": " + headers.get(headerKey));
+		}
+
+		return "Hello Brasov";
+	}
 
 }
--- lab-6-api-gateway/service2/src/main/java/com/example/service2/Service2Application.java
+++ lab-6-api-gateway/service2/src/main/java/com/example/service2/Service2Application.java
@@ -1,10 +1,34 @@
 package com.example.service2;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class Service2Application {
+	@Getter
+	@Setter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	static
+	class Product{
+		private String name;
+		private Integer quantity;
+	}
+
+	public static List<Product> productList = new ArrayList<>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(Service2Application.class, args);
@@ -16,4 +40,20 @@ public class Service2Application {
 	// 4. print request headers
 	// 5. register the service in eureka
 	// 6. define a GET endpoint /product to return the saved data using the POST endpoint - return type is List<Product>
+
+	@PostMapping("/product")
+	@ResponseStatus(HttpStatus.OK)
+	public Product saveProduct(@RequestHeader HttpHeaders headers, @RequestBody Product product){
+		for(String headerKey : headers.keySet()){
+			System.out.println(headerKey + ": " + headers.get(headerKey));
+		}
+
+		Service2Application.productList.add(product);
+		return product;
+	}
+
+	@GetMapping("/product")
+	public List<Product> getProductList(){
+		return Service2Application.productList;
+	}
 }
